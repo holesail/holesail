bae586d4fe4d857edad8628a045b1b8b1349b65d
supersuryaansh <prod.genx@gmail.com>
2024-03-07 13:18:51 +0530

    Added feature to connect to peers

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const DHT = require('holesail-server') //require module to start server on local port
 const goodbye = require('graceful-goodbye')
 const argv = require('minimist')(process.argv.slice(2)) //required to parse cli arguments
-const helpMessage = 'Usage: The command below will expose your local port to the network\nholesail --live port'
+const helpMessage = 'Usage: The command below will expose your local port to the network\nholesail --live port \n Command to connect to a holesail-server:\n holesail --connect <seed> --port <portno>'
 
 //setting up the command hierarchy
 if (argv.help) {
@@ -16,6 +16,18 @@ if (argv.live) {
         console.log('Server public key:', localServer.getPublicKey());
     });
 
+} else if (argv.connect) {
+
+    if(!argv.port){
+        port = 8989
+    }else{
+        port = argv.port
+    }
+    const holesailClient = require('holesail-client')
+    const pubClient = new holesailClient(argv.connect)
+    pubClient.connect(port, "127.0.0.1", () => {
+        console.log(`Listening on 127.0.0.1:${port}`)
+    })
 } else {
     console.log(helpMessage);
     process.exit(-1)

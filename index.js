be2624e79e5450e1257eea7fc33ed5375d767ccf
supersuryaansh <prod.genx@gmail.com>
2024-04-03 23:10:34 +0530

    Accessibility commit

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const DHT = require('holesail-server') //require module to start server on local
 const goodbye = require('graceful-goodbye')
 const HyperDHT = require('hyperdht')
 const argv = require('minimist')(process.argv.slice(2)) //required to parse cli arguments
+var term = require( 'terminal-kit' ).terminal ; //to beautify terminal output
+
+
 const {
     createHash
 } = require('crypto'); //for connectors
+
+//splitting into files
+const help = require('./includes/help.js');
+
+
 //setting up the command hierarchy
 if (argv.help) {
-    const help = require('./includes/help.js');
     help.printHelp(help.helpMessage);
     process.exit(-1)
 }
@@ -26,36 +33,44 @@ if (argv.live) {
     //to preserve seed
     if (argv.connector) {
         if (argv.connector.length === 64) {
-            connector = argv.connector
+            connector = argv.connector;
+            isConnectorSet = false;
         } else {
             connector = createHash('sha256').update(argv.connector.toString()).digest('hex');
+            isConnectorSet = true;
         }
 
     } else {
-        connector = null
+        connector = null;
+        isConnectorSet = false;
     }
 
     localServer.serve(argv.live, host, () => {
+
+        if (isConnectorSet) {
+            console.log(`Your connector is: ${argv.connector}`);
+        } else {
+            term.yellow("Notice: There is no connector set. \n");
+        }
+
         console.log(`Server started, Now listening on ${host}:` + argv.live);
-        console.log(`Your connector is: ${argv.connector}`);
         console.log('Server public key:', localServer.getPublicKey());
     }, connector);
 
 } else if (argv.connect) {
 
-    //give priority to connector instead of connection seed
-    if (argv.connector) {
-        if (argv.connector.length === 64) {
-            connector = argv.connector
-        } else {
-            connector = createHash('sha256').update(argv.connector.toString()).digest('hex');
-            const seed = Buffer.from(connector, 'hex');
-            //the keypair here is not a reference to the function above
-            connector = HyperDHT.keyPair(seed).publicKey.toString('hex');
-        }
+    //logic for holesail --connect key|seed
+    //key has priority over seed
 
+    if (argv.connect.length === 64) {
+        connector = argv.connect;
+        isConnectorSet = false;
     } else {
-        connector = argv.connect
+        connector = createHash('sha256').update(argv.connect.toString()).digest('hex');
+        const seed = Buffer.from(connector, 'hex');
+        //the keypair here is not a reference to the function above
+        connector = HyperDHT.keyPair(seed).publicKey.toString('hex');
+        isConnectorSet = true;
     }
 
     if (!argv.port) {
@@ -73,13 +88,61 @@ if (argv.live) {
     const holesailClient = require('holesail-client')
     const pubClient = new holesailClient(connector)
     pubClient.connect(port, host, () => {
-        console.log(`Client setup, access on ${host}:${port}`);
-        console.log(`Your connector is: ${argv.connector}`);
-        console.log('Connected to public key:', connector);
+            console.log(`Client setup, access on http://${host}:${port}/`);
+
+            if (isConnectorSet) {
+                console.log(`Your connector is: ${argv.connect}`);
+            } else {
+                term.yellow("Notice: There is no connector set.\n");
+            }
+
+            console.log('Connected to public key:', connector);
         }
     )
+} else if (argv['_'][0]) {
+
+    //logic for holesail key|connector
+
+    if (argv['_'][0].length === 64) {
+        connector = argv['_'][0];
+        isConnectorSet = false;
+    } else {
+        connector = createHash('sha256').update(argv['_'][0].toString()).digest('hex');
+        const seed = Buffer.from(connector, 'hex');
+        //the keypair here is not a reference to the function above
+        connector = HyperDHT.keyPair(seed).publicKey.toString('hex');
+        isConnectorSet = true;
+    }
+
+    if (!argv.port) {
+        port = 8989
+    } else {
+        port = argv.port
+    }
+    //--host
+    if (argv.host) {
+        host = argv.host
+    } else {
+        host = '127.0.0.1'
+    }
+
+    const holesailClient = require('holesail-client')
+    const pubClient = new holesailClient(connector)
+    pubClient.connect(port, host, () => {
+            console.log(`Client setup, access on http://${host}:${port}/`);
+
+            if (isConnectorSet) {
+                console.log(`Your connector is: ${argv['_'][0]}`);
+            } else {
+                term.yellow("Notice: There is no connector set.\n");
+            }
+
+            console.log('Connected to public key:', connector);
+        }
+    )
+
 } else {
-    console.log(helpMessage);
+    help.printHelp(help.helpMessage);
     process.exit(-1)
 }
 

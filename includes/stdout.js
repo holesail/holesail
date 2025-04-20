1fcecb53758b432f41a8518cc2d187b6b118bd39
supersuryaansh <prod.genx@gmail.com>
2025-04-20 13:20:29 +0530

    wip

--- includes/stdout.js
+++ includes/stdout.js
@@ -3,12 +3,7 @@ const boxConsole = require('cli-box')
 
 function stdout (protocol, type, secure, host, port, key, livefiles) {
 
-  if (livefiles) {
-
-  }
-
-const _HEADING = colors.cyan(colors.underline(colors.bold(`Holesail ${ livefiles ? 'Filemanager' : `${protocol.toUpperCase()} ${type[0].toUpperCase() + type.slice(1)}` } Started`)))
-
+  const _HEADING = colors.cyan(colors.underline(colors.bold(`Holesail ${livefiles ? 'Filemanager' : `${protocol.toUpperCase()} ${type[0].toUpperCase() + type.slice(1)}`} Started`)))
 
   const _MODE = secure ? colors.green('Private Connection String') : colors.yellow('Public Connection String')
 
@@ -16,7 +11,7 @@ const _HEADING = colors.cyan(colors.underline(colors.bold(`Holesail ${ livefiles
 
   const _KEY = type === 'server' ? `Connect with key: ${colors.dim(key)}` : `Connected to key: ${colors.dim(key)}`
 
-  const _INFO = secure ? colors.dim(`   NOTE: TREAT PRIVATE CONNECTION STRINGS HOW YOU WOULD TREAT SSH KEY, DO NOT SHARE IT WITH ANYONE YOU DO NOT TRUST    `) :colors.dim(`   NOTICE: TREAT PUBLIC STRING LIKE YOU WOULD TREAT A DOMAIN NAME ON PUBLIC SERVER, IF THERE IS ANYTHING PRIVATE ON IT, IT IS YOUR RESPONSIBILITY TO PASSWORD PROTECT IT OR USE PRIVATE MODE   \n`)
+  const _INFO = secure ? colors.dim(`   NOTE: TREAT PRIVATE CONNECTION STRINGS HOW YOU WOULD TREAT SSH KEY, DO NOT SHARE IT WITH ANYONE YOU DO NOT TRUST    `) : colors.dim(`   NOTICE: TREAT PUBLIC STRING LIKE YOU WOULD TREAT A DOMAIN NAME ON PUBLIC SERVER, IF THERE IS ANYTHING PRIVATE ON IT, IT IS YOUR RESPONSIBILITY TO PASSWORD PROTECT IT OR USE PRIVATE MODE   \n`)
 
   const box = boxConsole('100x10', {
       text: _HEADING + ' ⛵️' + '\n' +
@@ -35,5 +30,5 @@ const _HEADING = colors.cyan(colors.underline(colors.bold(`Holesail ${ livefiles
 
 }
 
-stdout('udp', 'client', true, '127.0.0.1', 3434, 'f05d903fb2e99f9c2ad38f296126dcf14e95eab7b4d4918aba1f771bca97e551')
+stdout('tcp', 'server', false, '127.0.0.1', 3434, 'f05d903fb2e99f9c2ad38f296126dcf14e95eab7b4d4918aba1f771bca97e551')
 

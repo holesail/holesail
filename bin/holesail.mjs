5a6e5b9c0ff6813289dd84a62388598765de15b2
Codesamp-Rohan <rohanchaudhary.bkbiet2022@gmail.com>
2025-05-13 20:18:01 +0530

    removed package-lock

--- bin/holesail.mjs
+++ bin/holesail.mjs
@@ -33,83 +33,83 @@ if (argv.version) {
 }
 
 // Set a port live
-// if (argv.live) {
-//   let secure
-//   if (argv.public !== undefined) {
-//     secure = !argv.public
-//   } else {
-//     secure = true
-//   }
-//
-//   const conn = new Holesail({
-//     server: true,
-//     port: argv.live,
-//     host: argv.host || '127.0.0.1',
-//     udp: argv.udp,
-//     secure,
-//     key: argv.key
-//   })
-//   await conn.ready()
-//   const info = conn.info
-//   stdout(info.protocol, info.type, info.secure, info.host, info.port, info.url)
-// } else if (argv.connect || argv._[0]) {
-//   const key = argv.connect || argv._[0]
-//
-//   const conn = new Holesail({
-//     client: true,
-//     port: argv.port,
-//     host: argv.host,
-//     key,
-//     udp: argv.udp,
-//     secure: argv.public
-//   })
-//   await conn.ready()
-//   const info = conn.info
-//   stdout(info.protocol, info.type, info.secure, info.host, info.port, info.url)
-// } else if (argv.filemanager) { // Start server with a filemanager
-//   const fileOptions = {
-//     path: argv.filemanager,
-//     role: argv.role,
-//     username: argv.username,
-//     password: argv.password,
-//     host: argv.host,
-//     port: argv.port
-//   }
-//
-//   // Start files server
-//   const fileServer = new Livefiles(fileOptions)
-//   await fileServer.ready()
-//   const fsInfo = fileServer.info
-//
-//   let secure
-//   if (argv.public !== undefined) {
-//     secure = !argv.public
-//   } else {
-//     secure = true
-//   }
-//
-//   const connOptions = {
-//     server: true,
-//     port: argv.port || 5409,
-//     host: argv.host || '127.0.0.1',
-//     secure,
-//     key: argv.key
-//   }
-//   const conn = new Holesail(connOptions)
-//   await conn.ready()
-//
-//   const dhtInfo = conn.info
-//   stdout(dhtInfo.protocol, fsInfo.type, dhtInfo.secure, dhtInfo.host, dhtInfo.port, dhtInfo.url, {
-//     username: fsInfo.username,
-//     password: fsInfo.password,
-//     role: fsInfo.role
-//   })
-//   // destroy before exiting
-//   goodbye(async () => {
-//     await conn.close()
-//     await fileServer.close()
-//   })
-// } else { // Default if no correct option is chosen
-//   printHelp(argv.help)
-//   process.exit(0)
-// }
+if (argv.live) {
+  let secure
+  if (argv.public !== undefined) {
+    secure = !argv.public
+  } else {
+    secure = true
+  }
+
+  const conn = new Holesail({
+    server: true,
+    port: argv.live,
+    host: argv.host || '127.0.0.1',
+    udp: argv.udp,
+    secure,
+    key: argv.key
+  })
+  await conn.ready()
+  const info = conn.info
+  stdout(info.protocol, info.type, info.secure, info.host, info.port, info.url)
+} else if (argv.connect || argv._[0]) {
+  const key = argv.connect || argv._[0]
+
+  const conn = new Holesail({
+    client: true,
+    port: argv.port,
+    host: argv.host,
+    key,
+    udp: argv.udp,
+    secure: argv.public
+  })
+  await conn.ready()
+  const info = conn.info
+  stdout(info.protocol, info.type, info.secure, info.host, info.port, info.url)
+} else if (argv.filemanager) { // Start server with a filemanager
+  const fileOptions = {
+    path: argv.filemanager,
+    role: argv.role,
+    username: argv.username,
+    password: argv.password,
+    host: argv.host,
+    port: argv.port
+  }
+
+  // Start files server
+  const fileServer = new Livefiles(fileOptions)
+  await fileServer.ready()
+  const fsInfo = fileServer.info
+
+  let secure
+  if (argv.public !== undefined) {
+    secure = !argv.public
+  } else {
+    secure = true
+  }
+
+  const connOptions = {
+    server: true,
+    port: argv.port || 5409,
+    host: argv.host || '127.0.0.1',
+    secure,
+    key: argv.key
+  }
+  const conn = new Holesail(connOptions)
+  await conn.ready()
+
+  const dhtInfo = conn.info
+  stdout(dhtInfo.protocol, fsInfo.type, dhtInfo.secure, dhtInfo.host, dhtInfo.port, dhtInfo.url, {
+    username: fsInfo.username,
+    password: fsInfo.password,
+    role: fsInfo.role
+  })
+  // destroy before exiting
+  goodbye(async () => {
+    await conn.close()
+    await fileServer.close()
+  })
+} else { // Default if no correct option is chosen
+  printHelp(argv.help)
+  process.exit(0)
+}

a77f2b20bcc39fb1c3f45604a8b4f3ce4fe8f1f8
Rohan Chaudhary <121369255+Codesamp-Rohan@users.noreply.github.com>
2025-05-18 17:55:35 +0530

    Drop support for PM2

--- bin/holesail.mjs
+++ bin/holesail.mjs
@@ -32,129 +32,84 @@ if (argv.version) {
   process.exit(0)
 }
 
-if (argv.list || argv.delete || argv.stop || argv.start || argv.background || argv.logs) {
-  if (runtime === 'bare') {
-    console.log('Info: Running Holesail in background is not supported on bare')
-    process.exit(0)
+// Set a port live
+if (argv.live) {
+  let secure
+  if (argv.public !== undefined) {
+    secure = !argv.public
+  } else {
+    secure = true
   }
 
-  const { PM2list, PM2delete, PM2stop, PM2start, PM2create, PM2logs } = await import('barely-pm2')
-  if (argv.list) {
-    PM2list({ raw: true, name: 'holesail' })
+  const conn = new Holesail({
+    server: true,
+    port: argv.live,
+    host: argv.host || '127.0.0.1',
+    udp: argv.udp,
+    secure,
+    key: argv.key
+  })
+  await conn.ready()
+  const info = conn.info
+  stdout(info.protocol, info.type, info.secure, info.host, info.port, info.url)
+} else if (argv.connect || argv._[0]) {
+  const key = argv.connect || argv._[0]
+
+  const conn = new Holesail({
+    client: true,
+    port: argv.port,
+    host: argv.host,
+    key,
+    udp: argv.udp,
+    secure: argv.public
+  })
+  await conn.ready()
+  const info = conn.info
+  stdout(info.protocol, info.type, info.secure, info.host, info.port, info.url)
+} else if (argv.filemanager) { // Start server with a filemanager
+  const fileOptions = {
+    path: argv.filemanager,
+    role: argv.role,
+    username: argv.username,
+    password: argv.password,
+    host: argv.host,
+    port: argv.port
   }
 
-  if (argv.delete) {
-    PM2delete(argv.delete)
+  // Start files server
+  const fileServer = new Livefiles(fileOptions)
+  await fileServer.ready()
+  const fsInfo = fileServer.info
+
+  let secure
+  if (argv.public !== undefined) {
+    secure = !argv.public
+  } else {
+    secure = true
   }
 
-  if (argv.stop) {
-    PM2stop(argv.stop)
+  const connOptions = {
+    server: true,
+    port: argv.port || 5409,
+    host: argv.host || '127.0.0.1',
+    secure,
+    key: argv.key
   }
+  const conn = new Holesail(connOptions)
+  await conn.ready()
 
-  if (argv.start) {
-    PM2start(argv.start)
-  }
-
-  if (argv.logs) {
-    PM2logs(argv.logs)
-  }
-
-  if (argv.background) {
-    const arr = ['list', 'delete', 'stop', 'start', 'background']
-    arr.forEach(key => {
-      delete argv[key]
-    })
-
-    const scriptArgs = Object.entries(argv).flatMap(([key, value]) => {
-      return key === '_' ? value : [`--${key}`, value]
-    })
-
-    const name = argv.name ? 'holesail-' + argv.name : `holesail-${Date.now()}`
-
-    PM2create({
-      name,
-      script: __filename,
-      args: scriptArgs,
-      timeout: '5000'
-    })
-  }
-} else {
-  // Set a port live
-  if (argv.live) {
-    let secure
-    if (argv.public !== undefined) {
-      secure = !argv.public
-    } else {
-      secure = true
-    }
-
-    const conn = new Holesail({
-      server: true,
-      port: argv.live,
-      host: argv.host || '127.0.0.1',
-      udp: argv.udp,
-      secure,
-      key: argv.key
-    })
-    await conn.ready()
-    const info = conn.info
-    stdout(info.protocol, info.type, info.secure, info.host, info.port, info.url)
-  } else if (argv.connect || argv._[0]) {
-    const key = argv.connect || argv._[0]
-
-    const conn = new Holesail({
-      client: true,
-      port: argv.port,
-      host: argv.host,
-      key,
-      udp: argv.udp,
-      secure: argv.public
-    })
-    await conn.ready()
-    const info = conn.info
-    stdout(info.protocol, info.type, info.secure, info.host, info.port, info.url)
-  } else if (argv.filemanager) { // Start server with a filemanager
-    const fileOptions = {
-      path: argv.filemanager,
-      role: argv.role,
-      username: argv.username,
-      password: argv.password,
-      host: argv.host,
-      port: argv.port
-    }
-
-    // Start files server
-    const fileServer = new Livefiles(fileOptions)
-    await fileServer.ready()
-    const fsInfo = fileServer.info
-
-    let secure
-    if (argv.public !== undefined) {
-      secure = !argv.public
-    } else {
-      secure = true
-    }
-
-    const connOptions = {
-      server: true,
-      port: argv.port || 5409,
-      host: argv.host || '127.0.0.1',
-      secure,
-      key: argv.key
-    }
-    const conn = new Holesail(connOptions)
-    await conn.ready()
-
-    const dhtInfo = conn.info
-    stdout(dhtInfo.protocol, fsInfo.type, dhtInfo.secure, dhtInfo.host, dhtInfo.port, dhtInfo.url, { username: fsInfo.username, password: fsInfo.password, role: fsInfo.role })
-
-    // destroy before exiting
-    goodbye(async () => {
-      await conn.close()
-      await fileServer.close()
-    })
-  } else { // Default if no correct option is chosen
-    printHelp(argv.help)
-    process.exit(0)
-  }
+  const dhtInfo = conn.info
+  stdout(dhtInfo.protocol, fsInfo.type, dhtInfo.secure, dhtInfo.host, dhtInfo.port, dhtInfo.url, {
+    username: fsInfo.username,
+    password: fsInfo.password,
+    role: fsInfo.role
+  })
+  // destroy before exiting
+  goodbye(async () => {
+    await conn.close()
+    await fileServer.close()
+  })
+} else { // Default if no correct option is chosen
+  printHelp(argv.help)
+  process.exit(0)
 }

946a7ec518ce440cd4a5567592102c8fbff5c5b2
Raven Scott <snxraven@gmail.com>
2025-08-01 00:51:33 +0000

    Adding logging

--- bin/holesail.mjs
+++ bin/holesail.mjs
@@ -1,23 +1,22 @@
 #!/usr/bin/env node
+
 import { runtime } from 'which-runtime'
 import process from 'process'
 import minimist from 'minimist' // Required to parse CLI arguments
 import goodbye from 'graceful-goodbye'
 import Holesail from '../index.js'
 import Livefiles from 'livefiles'
-
 import printHelp from '../lib/help.js'
 import { validateInput } from '../lib/validateInput.js'
 import stdout from '../lib/stdout.js'
-
 import { fileURLToPath } from 'url'
 import { createRequire } from 'node:module'
-
 import colors from 'barely-colours'
 
 const __filename = fileURLToPath(import.meta.url)
 const require = createRequire(import.meta.url)
 const { version } = require('../package.json')
+
 const argv = minimist(process.argv.slice(2))
 
 validateInput(argv)
@@ -42,28 +41,28 @@ if (argv.live) {
   } else {
     secure = true
   }
-
   const conn = new Holesail({
     server: true,
     port: argv.live,
     host: argv.host || '127.0.0.1',
     udp: argv.udp,
     secure,
-    key: argv.key
+    key: argv.key,
+    log: argv.log
   })
   await conn.ready()
   const info = conn.info
   stdout(info.protocol, info.type, info.secure, info.host, info.port, info.url)
 } else if (argv.connect || argv._[0]) {
   const key = argv.connect || argv._[0]
-
   const conn = new Holesail({
     client: true,
     port: argv.port,
     host: argv.host,
     key,
     udp: argv.udp,
-    secure: argv.public
+    secure: argv.public,
+    log: argv.log
   })
   await conn.ready()
   const info = conn.info
@@ -77,29 +76,26 @@ if (argv.live) {
     host: argv.host,
     port: argv.port
   }
-
   // Start files server
   const fileServer = new Livefiles(fileOptions)
   await fileServer.ready()
   const fsInfo = fileServer.info
-
   let secure
   if (argv.public !== undefined) {
     secure = !argv.public
   } else {
     secure = true
   }
-
   const connOptions = {
     server: true,
     port: argv.port || 5409,
     host: argv.host || '127.0.0.1',
     secure,
-    key: argv.key
+    key: argv.key,
+    log: argv.log
   }
   const conn = new Holesail(connOptions)
   await conn.ready()
-
   const dhtInfo = conn.info
   stdout(dhtInfo.protocol, fsInfo.type, dhtInfo.secure, dhtInfo.host, dhtInfo.port, dhtInfo.url, {
     username: fsInfo.username,
--- index.js
+++ index.js
@@ -3,36 +3,28 @@ const HolesailServer = require('holesail-server')
 const HolesailClient = require('holesail-client')
 const libKeys = require('hyper-cmd-lib-keys')
 const z32 = require('z32')
-
 const { validateOpts } = require('./lib/validateInput')
-
 const createHash = require('crypto').createHash
 
 class Holesail extends ReadyResource {
   constructor (opts = {}) {
     super()
-
     validateOpts(opts)
-
     this.server = opts.server || false
     this.client = opts.client || false
     this.port = opts.port
     this.host = opts.host
-
     const data = Holesail.urlParser(opts.key)
     if (data.secure === undefined) {
       this.secure = opts.secure
     } else {
       this.secure = data.secure
     }
-
     this.key = data.key
-
     this.udp = opts.udp
-
+    this.log = opts.log || false
     this.dht = null
     this.running = false
-
     this.#initialise()
   }
 
@@ -53,26 +45,21 @@ class Holesail extends ReadyResource {
     const protocol = 'hs://'
     let key
     let secure
-
     if (url && url.substring(0, 5) === protocol && url.substring(5, 9).length === 4) {
       key = url.substring(9)
     } else {
       key = url
     }
-
     if (url && url.substring(5, 6) === 's') {
       secure = true
     }
-
     return { key, secure }
   }
 
   static async lookup (url) {
     const { key: keyStr, secure: specifiedSecure } = Holesail.urlParser(url)
     let keyForPing
-
     const isSecure = specifiedSecure !== undefined ? specifiedSecure : false
-
     if (isSecure) {
       const seedBuffer = createHash('sha256').update(keyStr).digest()
       keyForPing = z32.encode(seedBuffer)
@@ -84,29 +71,26 @@ class Holesail extends ReadyResource {
         throw new Error('Invalid key format.')
       }
     }
-
     return await HolesailClient.ping(keyForPing)
   }
 
   async _open () {
     if (this.server) {
-      this.dht = new HolesailServer()
+      this.dht = new HolesailServer({ log: this.log })
       await this.connect()
     } else {
-      this.dht = new HolesailClient({ key: this.seed, secure: this.secure })
+      this.dht = new HolesailClient({ key: this.seed, secure: this.secure, log: this.log })
       await this.connect()
     }
   }
 
   async connect () {
     if (this.running) throw new Error('Already connected')
-
     if (this.server) {
       await this.dht.start({ port: this.port, host: this.host, seed: this.seed, secure: this.secure, udp: this.udp })
     } else {
       await this.dht.connect({ port: this.port, host: this.host, udp: this.udp })
     }
-
     this.running = true
   }
 
@@ -126,20 +110,16 @@ class Holesail extends ReadyResource {
     } else {
       key = info.key
     }
-
     let url
-
     if (this.secure) {
       url = 'hs://' + 's000' + key
     } else {
       url = 'hs://' + '0000' + key
     }
-
     if (this.secure && this.client) {
       const key = this.key
       info.seed = createHash('sha256').update(key.toString()).digest('hex')
     }
-
     return {
       type: info.type,
       state: info.state,

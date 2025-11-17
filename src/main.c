10087d4b9f80db12a299e722e6327a4a08f739bd
supersuryaansh <prod.genx@gmail.com>
2025-11-17 11:02:31 +0530

    Update C code for bare teardown

--- src/main.c
+++ src/main.c
@@ -41,11 +41,11 @@ main(int argc, char *argv[]) {
   err = bare_load(bare, "/main.bundle", &source, NULL);
   assert(err == 0);
 
-  err = bare_run(bare);
+  err = bare_run(bare, UV_RUN_DEFAULT);
   assert(err == 0);
 
   int exit_code;
-  err = bare_teardown(bare, &exit_code);
+  err = bare_teardown(bare, UV_RUN_DEFAULT, &exit_code);
   assert(err == 0);
 
   err = js_destroy_platform(platform);

fc13cf2f4217d3870d99a284a6dcf9d003a60d33
supersuryaansh <prod.genx@gmail.com>
2026-04-05 09:25:10 +0530

    progress

--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
 const ReadyResource = require('ready-resource')
 const HolesailServer = require('holesail-server')
 const HolesailClient = require('holesail-client')
-const libKeys = require('hyper-cmd-lib-keys')
 const z32 = require('z32')
-const { validateOpts } = require('./lib/validateInput')
 const createHash = require('crypto').createHash
-const HolesailLogger = require('holesail-logger')
 
 class Holesail extends ReadyResource {
   constructor(opts = {}) {
     super()
-    validateOpts(opts)
     this.server = opts.server || false
     this.client = opts.client || false
     this.port = opts.port
     this.host = opts.host
-    const data = Holesail.urlParser(opts.key)
-    if (data.secure === undefined) {
-      this.secure = opts.secure
-    } else {
-      this.secure = data.secure
-    }
-    this.key = data.key
+    this.key = opts.key
+    this.logger = opts.logger
     this.udp = opts.udp
-    this.log = opts.log !== undefined ? opts.log : false
     this.dht = null
     this.running = false
     this.#initialise()
@@ -79,28 +69,14 @@ class Holesail extends ReadyResource {
   }
 
   async _open() {
-    let enabled = false
-    let level = 1 // Default to INFO level
-    if (typeof this.log === 'boolean') {
-      enabled = this.log
-      if (enabled) level = 1 // Enable INFO and above if log is true
-    } else if (typeof this.log === 'number') {
-      enabled = true
-      level = Math.max(0, Math.min(3, this.log)) // Clamp level between 0 (DEBUG) and 3 (ERROR)
-    }
-    const loggerOpts = { prefix: 'Holesail', enabled, level }
-    if (!this.server) {
-      loggerOpts.debug = this.log === 0
-    }
-    const logger = new HolesailLogger(loggerOpts)
     if (this.server) {
-      this.dht = new HolesailServer({ logger })
+      this.dht = new HolesailServer({ logger: this.logger })
       await this.connect()
     } else {
       this.dht = new HolesailClient({
         key: this.seed,
         secure: this.secure,
-        logger,
+        logger: this.logger,
         debug: this.log === 0
       })
       await this.connect()
